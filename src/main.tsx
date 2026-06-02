8598fe63761cad01fc0404fb3333b732f38337ef
Alexis Ukogu <aukogu@Alexiss-MacBook-Pro-2.local>
2026-06-02 10:47:26 -0400

    Add PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import posthog from "posthog-js";
+
+posthog.init("phc_oYp3FpSPRHPLCmQY4SP76VbPUkMaAvKfvQ8vszNtZfc2", {
+  api_host: "https://us.i.posthog.com",
+  capture_pageview: true,
+  capture_pageleave: true,
+  session_recording: {
+    maskAllInputs: false,
+  },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
